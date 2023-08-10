1f87cfb1b020422a2521e53f866a7da45734d428
Hong Jeonghyeon <1staries@naver.com>
2023-08-11 08:04:51 +0900

    [FE] Feat: 핀 번호 간단복사

--- frontend/src/pages/GameRoom/components/UI/PinNumberDisplay/PinNumberDisplay.jsx
+++ frontend/src/pages/GameRoom/components/UI/PinNumberDisplay/PinNumberDisplay.jsx
@@ -1,8 +1,15 @@
 import styles from './PinNumberDisplay.module.scss';
 
 export default function PinNumberDisplay(/** @type {string}*/ { pinNumber }) {
+  function handleClick() {
+    navigator.clipboard.writeText(pinNumber).then(() => {
+      alert('핀 번호가 클립보드에 저장되었습니다.');
+    });
+  }
+
   return (
-    <div className={`${styles.PinNumberDisplay}`}>
+    <div className={`${styles.PinNumberDisplay}`} onClick={handleClick}>
+      <div>핀 번호 저장</div>
       <div>{pinNumber}</div>
     </div>
   );
