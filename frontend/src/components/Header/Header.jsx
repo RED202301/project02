e922144fc36fa77cb34a6723a29365f55fad8ec0
홍진환 <ghdtka1218@gmail.com>
2023-08-17 21:50:21 +0900

    [FE] 디자인 수정

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,31 +6,7 @@ import {Link} from'react-router-dom';
 function Header(props){
     return(
         <div className="header">
-            <Link to='/Webmain'>
-            <div className='logo'>
-                로고
-            </div>
-            </Link>
-            <Link to='/main'>
-            <div className='game'>
-                게임
-            </div>
-            </Link>
-            <Link to='/CardFactory'>
-            <div className='cardfactory'>
-                카드 공방
-            </div>
-            </Link>
-            {/* <Link to='/Mypage'>
-            <div className='mypage'>
-                마이페이지
-            </div>
-            </Link> */}
-            <Link to=''>
-            <div className='logout'>
-                {/* <Logout/> */}
-            </div>
-            </Link>
+
         </div>
     );
 }
