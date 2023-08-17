b83b62294743fa480e5a206cb25da91d3c92526f
Hong Jeonghyeon <1staries@naver.com>
2023-08-17 17:41:46 +0900

    [FE] Design: ui 작업

--- frontend/src/pages/Main/Main.jsx
+++ frontend/src/pages/Main/Main.jsx
@@ -207,6 +207,9 @@ function Modal({ modal, setModal }) {
                     <option value={1}>1</option>
                     <option value={2}>2</option>
                     <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
+                    <option value={6}>6</option>
                   </select>
                 </div>
 
